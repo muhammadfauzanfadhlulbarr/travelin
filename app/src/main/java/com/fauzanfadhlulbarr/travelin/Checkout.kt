982003105446b55c7e157b50f71b0b3592166114
Muhammad Fauzan Fadhlulbarr <muhammadfauzanfadhlulbarr@gmail.com>
2022-08-18 00:54:22 +0700

    update

--- app/src/main/java/com/fauzanfadhlulbarr/travelin/Checkout.kt
+++ app/src/main/java/com/fauzanfadhlulbarr/travelin/Checkout.kt
@@ -1,15 +1,11 @@
 package com.fauzanfadhlulbarr.travelin
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 
-class HomeActivity : AppCompatActivity() {
+class Checkout : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
-        supportActionBar!!.hide()
-
+        setContentView(R.layout.activity_checkout)
     }
 }
